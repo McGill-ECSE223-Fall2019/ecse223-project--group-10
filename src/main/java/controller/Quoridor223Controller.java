16ae4ea00f296b5e1c786d83d8ee2636618049f0
Shuby_Mao <le-li.mao@mail.mcgill.ca>
2019-10-02 21:45:15 -0400

    generated the code and Created the controller class

---
+++ src/main/java/controller/Quoridor223Controller.java
@@ -0,0 +1,59 @@
+package controller;
+import java.sql.Time;
+public class Quoridor223Controller {
+	//under feature 1
+	public void createGame() {
+	
+	}
+	//under feature 2
+	public void selectUser(String playerName1, String playerName2) {
+		
+	}
+	//under feature 2
+	public void createUser(String name) {
+		
+	}
+	//under feature 3
+	public void setThinkingTime(Time thinkingTime, String playerName) {
+		
+	}
+	//under feature 4
+	public void initializeBoard() {
+		
+	}
+	//under feature 5
+	public void rotateWall() {
+		
+	}
+	//under feature 6
+	public void grabWall() {
+		
+	}
+	//under feature 7
+	public void moveWall() {
+		
+	}
+	//under feature 8
+	public void dropWall() {
+		
+	}
+	//under feature 9
+	public void savePosition() {
+		
+	}
+	//under feature 10
+	public void loadPosition() {
+	
+	}
+	//under feature 11
+	public void validatePosition() {
+		
+	}
+	//under feature 12
+	public void UpdateBoard() {
+		
+	}
+}
+	
+	
+
