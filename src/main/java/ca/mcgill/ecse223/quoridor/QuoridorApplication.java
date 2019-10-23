ae6d4d2a16b0d4922b0425c386cf8d2ba1404468
shubymao <le-li.mao@mail.mcgill.ca>
2019-10-23 16:16:58 -0400

    added view

--- src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
+++ src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
@@ -1,11 +1,14 @@
 package ca.mcgill.ecse223.quoridor;
 
 import ca.mcgill.ecse223.quoridor.model.Quoridor;
+import ca.mcgill.ecse223.quoridor.view.BoardComponent;
+import ca.mcgill.ecse223.quoridor.view.GamePage;
 
 public class QuoridorApplication {
 	private static Quoridor quoridor;
 	public static void main(String[] args) {
-
+		GamePage mainPage = new GamePage();
+		mainPage.setVisible(true);
 	}
 
 	public static Quoridor getQuoridor() {
