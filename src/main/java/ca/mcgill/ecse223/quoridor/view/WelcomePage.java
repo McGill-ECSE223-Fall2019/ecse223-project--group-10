7a7de61e3d177b802f33654eab013af4b259afa7
vanessa <veifrah@gmail.com>
2019-10-25 17:21:43 -0400

    Setting up structure.

--- src/main/java/ca/mcgill/ecse223/quoridor/view/WelcomePage.java
+++ src/main/java/ca/mcgill/ecse223/quoridor/view/WelcomePage.java
@@ -1,5 +1,18 @@
 package ca.mcgill.ecse223.quoridor.view;
 
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JSpinner;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.text.DateFormatter;
+import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+
+import java.sql.Time;
+import java.awt.Color;
+
 public class WelcomePage {
 	// TODO: implement the UI for this page
+	
+	
 }
