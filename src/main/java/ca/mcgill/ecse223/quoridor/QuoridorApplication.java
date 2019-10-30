2a225cc9e81a5a11bd42d439ff955965614a76c0
andrewta999 <andrewta365@gmail.com>
2019-10-30 16:08:55 -0400

    Implment Controller method for SetThinkingTime

--- src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
+++ src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
@@ -1,14 +1,16 @@
 package ca.mcgill.ecse223.quoridor;
 
 import ca.mcgill.ecse223.quoridor.model.Quoridor;
-import ca.mcgill.ecse223.quoridor.view.BoardComponent;
 import ca.mcgill.ecse223.quoridor.view.GamePage;
+import ca.mcgill.ecse223.quoridor.view.SetThinkingTimePage;
 
 public class QuoridorApplication {
 	private static Quoridor quoridor;
+	private static SetThinkingTimePage timePage;
+	private static GamePage game;
 	public static void main(String[] args) {
-		GamePage mainPage = new GamePage();
-		mainPage.setVisible(true);
+		timePage = new SetThinkingTimePage();
+		timePage.setVisible(true);
 	}
 
 	public static Quoridor getQuoridor() {
@@ -18,5 +20,12 @@ public class QuoridorApplication {
 		return quoridor;
 	}
 	
+	public static void setMainPage() {
+		if(timePage.getPageStatus()) {
+			timePage.setVisible(false);
+			game = new GamePage();
+			game.setVisible(true);
+		}
+	}
 	
 }
