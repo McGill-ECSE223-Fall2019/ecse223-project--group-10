2743e25f908152604509586cc85d16d38cabf842
vanessa <veifrah@gmail.com>
2019-10-31 22:10:34 -0400

    Fixed a last merge conflict. Now really up to date.

--- src/main/java/ca/mcgill/ecse223/quoridor/view/SetNamePage.java
+++ src/main/java/ca/mcgill/ecse223/quoridor/view/SetNamePage.java
@@ -131,16 +131,10 @@ public class SetNamePage extends JFrame {
 						writer.write("\n" + name2);
 					}
 					writer.close();
-<<<<<<< HEAD
 					Quoridor223Controller.setUser(name1);
 					Quoridor223Controller.setUser(name2);
 					Quoridor223Controller.createGame();
-=======
-					Quoridor223Controller.createGame();
-					Quoridor223Controller.createUser(name1);
-					Quoridor223Controller.createUser(name2);
 					Quoridor223Controller.creatPlayers();
->>>>>>> master
 					QuoridorApplication.setTimePage();
 					}
 				
