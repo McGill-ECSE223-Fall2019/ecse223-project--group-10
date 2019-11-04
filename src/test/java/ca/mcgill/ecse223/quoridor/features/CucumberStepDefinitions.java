2d550cbd5bf644b065397475165885770f4c1032
sachaStewart <sachalevy26@gmail.com>
2019-11-04 10:07:28 -0500

    add auto generated cucumber step def for movePlayer

--- src/test/java/ca/mcgill/ecse223/quoridor/features/CucumberStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse223/quoridor/features/CucumberStepDefinitions.java
@@ -1506,6 +1506,63 @@ public class CucumberStepDefinitions {
 		}
 	}
 	
+// MovePlayer feature
+	@Given("The player is located at {int}:{int}")
+	public void the_player_is_located_at(Integer int1, Integer int2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Given("There are no {string} walls {string} from the player")
+	public void there_are_no_walls_from_the_player(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Given("The opponent is not {string} from the player")
+	public void the_opponent_is_not_from_the_player(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("Player {string} initiates to move {string}")
+	public void player_initiates_to_move(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("The move {string} shall be {string}")
+	public void the_move_shall_be(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("Player's new position shall be {int}:{int}")
+	public void player_s_new_position_shall_be(Integer int1, Integer int2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("The next player to move shall become {string}")
+	public void the_next_player_to_move_shall_become(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Given("There is a {string} wall {string} from the player")
+	public void there_is_a_wall_from_the_player(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Given("My opponent is not {string} from the player")
+	public void my_opponent_is_not_from_the_player(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+	
+// ValidatePosition feature
+	
 	/**
 	 * ValidatePosition.feature
 	 * @author Sacha Lévy
